fc606724de5c9964a97c04dbd757ddddf12af80b
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2021-04-02 12:23:14 +0300

    deploy

--- ssg/source/client/index.ts
+++ ssg/source/client/index.ts
@@ -1,2 +1,40 @@
-import 'bootstrap';
+import 'bootstrap/js/src/modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import jQuery from 'jquery';
+
+jQuery(() => {
+  const $body = jQuery('body');
+  const $modal = jQuery(`<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title" id="modal-title">Modal title</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+    </div>`).appendTo($body);
+  const $modalTitle = $modal.find('.modal-title');
+  const $modalBody = $modal.find('.modal-body');
+  jQuery(`.albums li`).on('click', (item) => {
+    const $item = jQuery(item);
+    const title = $item.text();
+    const coverSource = $item.data('cover');
+    $modalTitle.text(title);
+    const tags = $item.data('tags');
+    $modalBody.html(`<div class="text-center">
+    <img src="${coverSource}" class="rounded mx-auto d-block" alt="${title}">
+    <p>${tags}</p>
+    </div>`);
+    $modal.modal();
+  });
+});
